35dde2154ee16104802482bc7a2cf304d83d7fcd
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-06 00:34:56 +0530

    basic app

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 8091, function () {
+  console.log("Server is up and running");
+});
