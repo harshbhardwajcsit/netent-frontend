bb59100406c8d91ec477f2b1fc268513f006c416
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-07 04:03:29 +0530

    deployment

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,13 +1,17 @@
 var pre_round_result = null;
-
+var bet = 0;
+var gain = 0;
 var coins;
+var playerName;
 
 function loadGame() {
   coins = 10000;
   document.getElementById("coins").innerHTML = coins;
+  takeUser();
 }
 
 function playGame() {
+  console.log(coins);
   document.getElementById("result").innerHTML = "";
   document.getElementsByClassName('spinner')[0].style.visibility = 'visible';
 
@@ -16,23 +20,30 @@ function playGame() {
     coins = coins - 10;
     console.log("coins before play game", coins);
     document.getElementById("coins").innerHTML = coins
+    bet = bet + 10;
   }
   pre_round_result = null;
-  fetchRandomResult(coins);
+  fetchRandomResult();
 }
 
 
-function evaluateGameResult(result, coins) {
+function evaluateGameResult(result) {
   document.getElementsByClassName('spinner')[0].style.visibility = 'hidden';
-  if (result.data.gameStatus === "LOST") {
-    document.getElementById("result").innerHTML = "Sorry you loose";
-  } else if (result.data.gameStatus === 'WIN') {
-    document.getElementById("result").innerHTML = "Congrats! you get 20 more coins";
-    coins = coins + 20;
-    document.getElementById("coins").innerHTML = coins
-  } else {
-    document.getElementById("result").innerHTML = "Hurry! you get extra free round to play";
-    pre_round_result = 'FREE_ROUND'
+  saveRecordByPlayer(result);
+  switch (result.data.gameStatus) {
+    case "LOST":
+      document.getElementById("result").innerHTML = "Sorry! you loose your coins";
+      break;
+    case "WIN":
+      coins = coins + 20;
+      document.getElementById("coins").innerHTML = coins;
+      document.getElementById("result").innerHTML = "Congrats! you get 20 more coins";
+      gain = gain + 20;
+      break;
+    case "FREE ROUND":
+      document.getElementById("result").innerHTML = "Hurry! you get extra free round to play";
+      pre_round_result = 'FREE_ROUND';
+      break;
   }
 }
 
@@ -40,7 +51,7 @@ function gameRules() {
   alert("you can bet 10 coin per round, if you win we give you 20 coins, but if loose 10 coins are gone now. you can also earn a bonus round where you don't have to bet 10 coins to play game");
 }
 
-function fetchRandomResult(coins) {
+function fetchRandomResult() {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
     var result = JSON.parse(this.responseText);
@@ -52,6 +63,27 @@ function fetchRandomResult(coins) {
   xhr.send();
 }
 
-function gameHistory() {
+function gameRTP() {
+  if (gain === 0) {
+    alert("NO RTP generated");
+  } else {
+    alert(gain / bet);
+  }
+}
 
+function saveRecordByPlayer(result) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    var result = JSON.parse(this.responseText);
+    if (result) {
+      console.log(result);
+    }
+  };
+  xhr.open("POST", "http://localhost:8080/api/coin/save/player", true);
+  xhr.send();
+}
+
+
+function takeUser() {
+  document.getElementById("name").innerHTML = prompt("What's your cool name?", "Mighty harsh");
 }
