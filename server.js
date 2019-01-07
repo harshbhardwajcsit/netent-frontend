40789539ab76cb91997e825e5414471bc62f63a8
Harsh Bhardwaj <hbhardwaj@ptc.com>
2019-01-07 17:23:34 +0530

    changes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/src/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 8090, function () {
+app.listen(process.env.PORT || 8091, function () {
   console.log("Server is up and running");
 });
